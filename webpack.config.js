31b2305cbcaf67529001464ad4abf96452b2fd16
Madhur <madhur.ahuja@helpchat.in>
2016-05-15 01:02:39 +0530

    countdown

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,8 @@ module.exports = {
 			Timer: 'app/components/Timer.jsx',
 			Countdown: 'app/components/Countdown.jsx',
 			Clock: 'app/components/Clock.jsx',
-			CountdownForm: 'app/components/CountdownForm.jsx'
+			CountdownForm: 'app/components/CountdownForm.jsx',
+			Controls: 'app/components/Controls.jsx'
 		},
 		
 		extensions: ['', '.jsx', '.js' ]
